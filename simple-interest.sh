985ffa361cf8c9b076594138e55478fe2f60ca23
FranDelgado <125986610+FranDelgadoL@users.noreply.github.com>
2023-07-26 16:30:06 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Francisco Delgado L.(student)
+   # FranDelgadoL (student)
    # Input:
    # p, principal amount
    # t, time period in years
